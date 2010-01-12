9078d3293cd2463a04dbda064bf8341e8c238062
Joshua Lippiner <jlippiner@joshua-lippiners-macbook-pro.local>
2010-01-12 10:12:58 -0500

    environment for deploy

--- config/compass.rb
+++ config/compass.rb
@@ -9,7 +9,8 @@ http_path = "/"
 css_dir = "stylesheets/compiled"
 sass_dir = "app/stylesheets"
 cache_dir = "tmp/sass-cache"
-images_dir = "images"
+images_dir = "images" 
+
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
