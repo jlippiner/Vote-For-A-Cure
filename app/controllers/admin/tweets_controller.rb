e12e42ae3158b17d6bd4e18d51a996104e8705b8
Joshua Lippiner <jlippiner@joshua-lippiners-macbook-pro.local>
2010-01-17 09:43:16 -0500

    ajax pagination

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,17 +1,24 @@
 class Admin::TweetsController < AdminController
-  before_filter :get_tweet, :except => [:index, :new, :create] 
-  
+  before_filter :get_tweet, :except => [:index, :new, :create]
+
   def index
-    @tweets = Tweet.paginate :page => params[:page], :per_page => 50, :order => "created_at DESC"
+    @tweets = Tweet.paginate :page => params[:page], :per_page => 25, :order => "created_at DESC"
+
+    respond_to do |format|
+      format.html
+      format.js {
+        render :partial => "list"
+      }
+    end
   end
-  
+
   def show
   end
-  
+
   def new
     @tweet = Tweet.new
   end
-  
+
   def create
     @tweet = Tweet.new(params[:tweet])
     if @tweet.save
@@ -21,11 +28,11 @@ class Admin::TweetsController < AdminController
       render :action => 'new'
     end
   end
-  
+
   def edit
 
   end
-  
+
   def update
     if @tweet.update_attributes(params[:tweet])
       flash[:notice] = "Successfully updated tweet."
@@ -34,16 +41,16 @@ class Admin::TweetsController < AdminController
       render :action => 'edit'
     end
   end
-  
+
   def destroy
     @tweet.destroy
     flash[:notice] = "Successfully destroyed tweet."
     redirect_to admin_tweets_url
   end
-  
-  private 
-  
-    def get_tweet
-      @tweet = Tweet.find_by_id(params[:id])
-    end
+
+  private
+
+  def get_tweet
+    @tweet = Tweet.find_by_id(params[:id])
+  end
 end
